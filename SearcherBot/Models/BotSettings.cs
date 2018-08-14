aa59c6c990866ab4e7bb17f92af3860cab8804f0
Elnur Alimirzayev <aliveit.elnur@gmail.com>
2018-08-14 08:56:06 +0400

    update

--- SearcherBot/Models/BotSettings.cs
+++ SearcherBot/Models/BotSettings.cs
@@ -8,23 +8,23 @@ namespace SearcherBot.Models
     public static class BotSettings
     {
 
-        public static string Url { get; set; } = "https://searcherbot.azurewebsites.net:443/{0}";
+        public static string Url { get; set; } = "";
 
-        public static string Name { get; set; } = "aliveit_searcher_bot";
+        public static string Name { get; set; } = "";
 
-        public static string Key { get; set; } = "603892572:AAFSzGa2rsiDd2ytnivVvfPAqAUubTovtzU";
+        public static string Key { get; set; } = "";
 
-        public static string GoogleApiKey { get; set; } = "AIzaSyC4r-3OEvr8s6zhRSrYH2jpGMXwk0di5Dw";
+        public static string GoogleApiKey { get; set; } = "";
 
-        public static string GoogleSearchEngineId { get; set; } = "009158199219010684512:y2b86stbkkw";
+        public static string GoogleSearchEngineId { get; set; } = "";
 
-        public static string DataSource { get; set; } = "searcherbot.database.windows.net";
+        public static string DataSource { get; set; } = "";
 
-        public static string UserID { get; set; } = "AliveIT";
+        public static string UserID { get; set; } = "";
 
-        public static string Password { get; set; } = "E1l2n3u4r5";
+        public static string Password { get; set; } = "";
 
-        public static string InitialCatalog { get; set; } = "SearcherBotDB";
+        public static string InitialCatalog { get; set; } = "";
 
     }
 }
